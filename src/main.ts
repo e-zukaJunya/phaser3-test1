82e89201c7ca1f294932d45313378627772645b1
Junya Iizuka <Junya_Iizuka@jmas.co.jp>
2024-05-04 13:45:15 +0900

    tmp

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,44 @@
-import './style.css'
-import style from './style2.module.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "phaser";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-    <p class="${style.moduleStyleTest}"}>aaa</p>
-  </div>
-`
+class Example extends Phaser.Scene {
+  preload() {
+    this.load.setBaseURL("https://labs.phaser.io");
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+    this.load.image("sky", "assets/skies/space3.png");
+    this.load.image("logo", "assets/sprites/phaser3-logo.png");
+    this.load.image("red", "assets/particles/red.png");
+  }
 
-// const styleDiv = document.getElementById('moduleStyleTest')!
-// styleDiv.setAttribute('class', 'moduleStyleTest')
+  create() {
+    this.add.image(400, 300, "sky");
+
+    const particles = this.add.particles(0, 0, "red", {
+      speed: 100,
+      scale: { start: 0, end: 0 },
+      blendMode: "ADD",
+    });
+
+    const logo = this.physics.add.image(400, 100, "logo");
+
+    logo.setVelocity(100, 100);
+    logo.setBounce(3, 3);
+    logo.setCollideWorldBounds(true);
+
+    particles.startFollow(logo);
+  }
+}
+
+const config = {
+  type: Phaser.AUTO,
+  width: 1200,
+  height: 700,
+  scene: Example,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { x: 200, y: 500 },
+    },
+  },
+};
+
+const game = new Phaser.Game(config);
