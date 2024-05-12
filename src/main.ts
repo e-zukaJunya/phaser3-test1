11264adf4a0b7047901f55976b13818f5e28a057
Junya Iizuka <Junya_Iizuka@jmas.co.jp>
2024-05-12 10:32:13 +0900

    2Dアクションゲーム基礎

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,27 @@ import 'phaser'
 
 let player: Phaser.Physics.Arcade.Sprite
 let cursors: Phaser.Types.Input.Keyboard.CursorKeys
+let score = 0
+let scoreText: Phaser.GameObjects.Text
+let endObj: Phaser.Physics.Arcade.Image
+let bombs: Phaser.Physics.Arcade.Group
 
 class Example extends Phaser.Scene {
     // 画面ロード前の実行ロジック
     preload() {
+        this.load.image('bg', 'src/assets/bg_2.jpg')
         this.load.image('bar', 'src/assets/bar.dio.png')
         this.load.image('melody', 'src/assets/music_file.jpg')
         this.load.image('kuya_shonin', 'src/assets/kuya_shonin_r.jpg')
+        this.load.image('chrome', 'src/assets/chrome_icon.png')
 
         cursors = this.input.keyboard!.createCursorKeys()
     }
 
     // 画面作成時の実行ロジック
     create() {
+        this.add.image(400, 300, 'bg').setScale(0.5)
+
         const platforms = this.physics.add.staticGroup()
         platforms.create(400, 568, 'bar').setScale(2).refreshBody()
         platforms.create(600, 400, 'bar')
@@ -70,6 +78,48 @@ class Example extends Phaser.Scene {
         const melody = this.physics.add.image(400, 100, 'melody').setScale(0.1)
         melody.setCollideWorldBounds(true)
         this.physics.add.collider(melody, platforms)
+
+        // キャラとオブジェクトの接触時の処理
+        this.physics.add.overlap(player, melody, () => {
+            melody.disableBody(true, true)
+            score += 10
+            scoreText.setText(`Score: ${score}`)
+        })
+
+        scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px' })
+
+        bombs = this.physics.add.group()
+        bombs.create(200, 500, 'chrome').setScale(0.2).setCollideWorldBounds(true)
+
+        this.physics.add.collider(bombs, platforms)
+
+        const hitBomb = () => {
+            // 停止
+            this.physics.pause()
+
+            // 死
+            player.setTint(0xff0000)
+
+            // 画面上をChromeが左から右に流れる
+            const curtain = this.add.image(-100, 300, 'chrome').setScale(2.5)
+            this.tweens.add({
+                targets: curtain,
+                x: { value: 1300, duration: 2000 },
+                repeat: -1, // 無限に繰り返す
+                // yoyo: true, // 左右を行き来する
+            })
+            // gameOver = true
+        }
+        // this.physics.add.collider(bombs, player)
+        this.physics.add.collider(player, bombs, hitBomb, undefined, this)
+        // endObj = this.physics.add.image(200, 500, 'chrome').setScale(0.2)
+        // endObj.setCollideWorldBounds(true)
+        // this.physics.add.collider(endObj, platforms)
+        // this.physics.add.overlap(player, endObj, () => {
+        //     // melody.disableBody(true, true)
+        //     score -= 10
+        //     scoreText.setText(`Score: ${score}`)
+        // })
     }
 
     // 画面更新時の実行ロジック
