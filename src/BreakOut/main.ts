3fb2eac54fc0274056b60626753503c761d97f88
Junya Iizuka <Junya_Iizuka@jmas.co.jp>
2024-05-12 12:30:16 +0900

    brick out

--- src/BreakOut/main.ts
+++ src/BreakOut/main.ts
@@ -1,158 +1,149 @@
-import 'phaser'
+// 必要なPhaserクラスをインポートします
+import Phaser from 'phaser'
 
-let player: Phaser.Physics.Arcade.Sprite
-let cursors: Phaser.Types.Input.Keyboard.CursorKeys
-let score = 0
-let scoreText: Phaser.GameObjects.Text
-let endObj: Phaser.Physics.Arcade.Image
-let bombs: Phaser.Physics.Arcade.Group
-
-class Example extends Phaser.Scene {
-    // 画面ロード前の実行ロジック
-    preload() {
-        this.load.image('bg', 'src/assets/bg_2.jpg')
-        this.load.image('bar', 'src/assets/bar.dio.png')
-        this.load.image('melody', 'src/assets/music_file.jpg')
-        this.load.image('kuya_shonin', 'src/assets/kuya_shonin_r.jpg')
-        this.load.image('chrome', 'src/assets/chrome_icon.png')
-
-        cursors = this.input.keyboard!.createCursorKeys()
-    }
-
-    // 画面作成時の実行ロジック
-    create() {
-        this.add.image(400, 300, 'bg').setScale(0.5)
-
-        const platforms = this.physics.add.staticGroup()
-        platforms.create(400, 568, 'bar').setScale(2).refreshBody()
-        platforms.create(600, 400, 'bar')
-        platforms.create(50, 250, 'bar')
-        platforms.create(750, 220, 'bar')
-
-        player = this.physics.add.sprite(100, 150, 'kuya_shonin').setScale(0.2)
-
-        player.setBounce(0.2)
-        // 世界の境界線との衝突
-        player.setCollideWorldBounds(true)
-        this.physics.add.collider(player, platforms)
-        if (player.body instanceof Phaser.Physics.Arcade.Body) {
-            player.body.setGravityY(200)
-        }
-
-        // this.anims.create({
-        //   key: "left",
-        //   frames: this.anims.generateFrameNumbers("kuya_shonin", { start: 0, end: 0 }),
-        //   frameRate: 10,
-        //   repeat: -1,
-        // });
-
-        // this.anims.create({
-        //     key: 'turn',
-        //     frames: [ { key: 'kuya_shonin', frame: 4 } ],
-        //     frameRate: 20
-        // });
-
-        // this.anims.create({
-        //   key: "right",
-        //   frames: this.anims.generateFrameNumbers("kuya_shonin", { start: 1, end: 1 }),
-        //   frameRate: 10,
-        //   repeat: -1,
-        // });
-        // this.add.image(400, 300, "sky");
-
-        // this.add.image(670, 380, "kuya_shonin").setScale(0.5);
-
-        // const logo = this.physics.add.image(400, 100, "logo");
-
-        // logo.setVelocity(100, 100);
-        // logo.setBounce(1, 1);
-        // logo.setCollideWorldBounds(true);
-
-        // const particles = this.add.particles(0, 0, "red", {
-        //     speed: 100,
-        //     scale: { start: 1, end: 0 },
-        //     blendMode: "ADD",
-        //   });
-
-        // particles.startFollow(logo);
-        const melody = this.physics.add.image(400, 100, 'melody').setScale(0.1)
-        melody.setCollideWorldBounds(true)
-        this.physics.add.collider(melody, platforms)
-
-        // キャラとオブジェクトの接触時の処理
-        this.physics.add.overlap(player, melody, () => {
-            melody.disableBody(true, true)
-            score += 10
-            scoreText.setText(`Score: ${score}`)
-        })
-
-        scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px' })
-
-        bombs = this.physics.add.group()
-        bombs.create(200, 500, 'chrome').setScale(0.2).setCollideWorldBounds(true)
-
-        this.physics.add.collider(bombs, platforms)
-
-        const hitBomb = () => {
-            // 停止
-            this.physics.pause()
-
-            // 死
-            player.setTint(0xff0000)
-
-            // 画面上をChromeが左から右に流れる
-            const curtain = this.add.image(-100, 300, 'chrome').setScale(2.5)
-            this.tweens.add({
-                targets: curtain,
-                x: { value: 1300, duration: 2000 },
-                repeat: -1, // 無限に繰り返す
-                // yoyo: true, // 左右を行き来する
-            })
-            // gameOver = true
-        }
-        // this.physics.add.collider(bombs, player)
-        this.physics.add.collider(player, bombs, hitBomb, undefined, this)
-        // endObj = this.physics.add.image(200, 500, 'chrome').setScale(0.2)
-        // endObj.setCollideWorldBounds(true)
-        // this.physics.add.collider(endObj, platforms)
-        // this.physics.add.overlap(player, endObj, () => {
-        //     // melody.disableBody(true, true)
-        //     score -= 10
-        //     scoreText.setText(`Score: ${score}`)
-        // })
-    }
-
-    // 画面更新時の実行ロジック
-    update(time: number, delta: number): void {
-        if (cursors.left.isDown) {
-            player.setVelocityX(-160)
-            // player.anims.play('left', true)
-        } else if (cursors.right.isDown) {
-            player.setVelocityX(160)
-            // player.anims.play('right', true)
-        } else {
-            player.setVelocityX(0)
-            // player.anims.play('turn')
-        }
-
-        if (cursors.up.isDown && player.body instanceof Phaser.Physics.Arcade.Body && player.body.touching.down) {
-            player.setVelocityY(-350)
-        }
-    }
-}
-
-const config = {
+// ゲームの設定を定義します
+const config: Phaser.Types.Core.GameConfig = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    scene: Example,
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { x: 0, y: 200 },
+            gravity: { x: 0, y: 0 },
+            debug: false,
         },
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update,
     },
 }
 
-const game = new Phaser.Game(config)
+// ゲームインスタンスを作成します
+new Phaser.Game(config)
+
+// ゲームの状態を保持する変数を定義します
+let ball: Phaser.Physics.Arcade.Sprite
+let paddle: Phaser.Physics.Arcade.Sprite
+let cursors: Phaser.Types.Input.Keyboard.CursorKeys
+let scoreText: Phaser.GameObjects.Text
+let score = 0
+
+function preload(this: Phaser.Scene) {
+    // アセットをロードします
+    this.load.image('ball', '/src/BreakOut/assets/ball.dio.png')
+    this.load.image('paddle', '/src/BreakOut/assets/paddle.dio.png')
+    this.load.image('brick', '/src/BreakOut/assets/brick.dio.png')
+}
+
+function create(this: Phaser.Scene) {
+    // ボールを作成します
+    ball = this.physics.add.sprite(400, 500, 'ball')
+    ball.setCollideWorldBounds(true)
+    ball.setBounce(1)
+    ball.setData('onPaddle', true)
+
+    // パドルを作成します
+    paddle = this.physics.add.sprite(400, 550, 'paddle')
+    paddle.setImmovable(true)
+    paddle.setCollideWorldBounds(true)
+
+    // スコアテキストを作成します
+    scoreText = this.add.text(16, 16, 'Score: 0', {
+        fontSize: '20px',
+        color: '#ffffff',
+    })
+
+    // キーボード入力を設定します
+    cursors = this.input.keyboard!.createCursorKeys()
+
+    // ブロックを作成します
+    const bricks = this.physics.add.staticGroup()
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 5; j++) {
+            bricks.create(80 + i * 64, 50 + j * 32, 'brick')
+        }
+    }
+
+    // コリジョンを設定します
+    this.physics.add.collider(ball, bricks, hitBrick, undefined, this)
+    this.physics.add.collider(ball, paddle, hitPaddle, undefined, this)
+
+    // スペースキーでボールを発射します
+    this.input.keyboard!.on('keydown-SPACE', function (event: any) {
+        if (ball.getData('onPaddle')) {
+            ball.setVelocity(-75, -300)
+            ball.setData('onPaddle', false)
+        }
+    })
+}
+
+function update(this: Phaser.Scene) {
+    // パドルの移動を制御します
+    paddle.x = this.input.x || 400
+
+    if (ball.getData('onPaddle')) {
+        ball.x = paddle.x
+    }
+
+    if (cursors.left.isDown) {
+        paddle.setVelocityX(-500)
+    } else if (cursors.right.isDown) {
+        paddle.setVelocityX(500)
+    } else {
+        paddle.setVelocityX(0)
+    }
+
+    // ゲームのルールを実装します
+    if (ball.y > 600) {
+        resetBall()
+    }
+}
+
+function hitBrick(
+    ball: Phaser.Types.Physics.Arcade.GameObjectWithBody | Phaser.Tilemaps.Tile,
+    brick: Phaser.Types.Physics.Arcade.GameObjectWithBody | Phaser.Tilemaps.Tile
+) {
+    // brickがPhaser.Types.Physics.Arcade.GameObjectWithBodyかどうかの判定
+    if (brick instanceof Phaser.Physics.Arcade.Sprite) {
+        brick.disableBody(true, true)
+    }
+    console.log(typeof brick)
+
+    // スコアを更新します
+    score += 10
+    scoreText.setText(`Score: ${score}`)
+
+    return true
+}
+
+function hitPaddle(
+    ball: Phaser.Types.Physics.Arcade.GameObjectWithBody | Phaser.Tilemaps.Tile,
+    brick: Phaser.Types.Physics.Arcade.GameObjectWithBody | Phaser.Tilemaps.Tile
+) {
+    console.log(typeof ball)
+
+    // brickがPhaser.Types.Physics.Arcade.GameObjectWithBodyかどうかの判定
+    if (ball instanceof Phaser.Physics.Arcade.Sprite) {
+        if (ball.x < paddle.x) {
+            // ボールがパドルの左側に当たった場合
+            const diff = paddle.x - ball.x
+            ball.setVelocityX(-10 * diff)
+        } else if (ball.x > paddle.x) {
+            // ボールがパドルの右側に当たった場合
+            const diff = ball.x - paddle.x
+            ball.setVelocityX(10 * diff)
+        } else {
+            // ボールがパドルの中央に当たった場合
+            ball.setVelocityX(2 + Math.random() * 8)
+        }
+    }
+}
+
+function resetBall() {
+    ball.setVelocity(0)
+    ball.setPosition(paddle.x, 500)
+    ball.setData('onPaddle', true)
+}
 
